42356ffc175778c21f47a88d9ae5b59a0f6c063f
yurytolochko <yury.tolochko@weavora.com>
2012-01-19 18:29:24 +0300

    Yii::app()->end() handling

---
+++ Http/YiiExitException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: yury.tolochko
+ * Date: 19.01.12
+ * Time: 10:22
+ * To change this template use File | Settings | File Templates.
+ */
+class YiiExitException extends Exception
+{
+
+}
