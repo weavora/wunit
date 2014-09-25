96f921304f221c2fb44f60ff9069f216c30d151b
Axel Kelting <axel.kelting@gmail.com>
2014-09-25 14:41:33 +0200

    Update test configuration for composer autoloading

--- Tests/fixtures/bootstrap.php
+++ Tests/fixtures/bootstrap.php
@@ -2,7 +2,7 @@
 
 error_reporting( E_ALL | E_STRICT );
 
-require(dirname(__FILE__).'/../../../../../../framework/yiit.php');
+require(dirname(__FILE__) .'/../../vendor/yiisoft/yii/framework/yiit.php');
 require(dirname(__FILE__) . '/../../WUnit.php');
 
 WUnit::createWebApplication(require(dirname(__FILE__) . '/config/main.php'));
